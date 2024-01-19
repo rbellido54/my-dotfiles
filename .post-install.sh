f7dc2a8dc7dac723ec493b9f3f3b4a067aa39a4d
Ron B <35236981+ronb54@users.noreply.github.com>
2024-01-18 21:46:13 -0800

    Add dots

---
+++ .post-install.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+python3 -m pip install --user --upgrade pynvim
+
+# Ruby RVM w/ Rails
+gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable --rails
+
+
+# oh-my-zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
