89e3956cb87316fe943b00222a83ef9bdc3be66f
Ron B <35236981+ronb54@users.noreply.github.com>
2024-01-18 21:46:14 -0800

    New path exports

--- .post-install.sh
+++ .post-install.sh
@@ -5,6 +5,9 @@ python3 -m pip install --user --upgrade pynvim
 # rbenv
 echo 'eval "$(~/.rbenv/bin/rbenv init - zsh)"' >> ~/.zshrc
 
+# nodeenv
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
 # oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
