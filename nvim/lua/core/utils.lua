5d939446940614f49a4fa9d4121cb94c3654c848
Ron B <35236981+ronb54@users.noreply.github.com>
2024-04-11 19:58:20 -0700

    [wip] work on keymap

--- nvim/lua/core/utils.lua
+++ nvim/lua/core/utils.lua
@@ -1,3 +1,7 @@
 local function cmd(command)
-   return table.concat({ '<Cmd>', command, '<CR>' })
+  return table.concat({ '<Cmd>', command, '<CR>' })
+end
+
+local function set_keymap(...)
+  vim.api.nvim_set_keymap(...)
 end
