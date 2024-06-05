c294beef56294a2974f274323ccd2956b4ab1a1b
Ron B <35236981+ronb54@users.noreply.github.com>
2024-06-05 01:05:05 -0700

    Lua formatter
    Signed-off-by: Ron B <35236981+ronb54@users.noreply.github.com>

--- nvim/lua/plug-config/formatter.lua
+++ nvim/lua/plug-config/formatter.lua
@@ -7,22 +7,23 @@ require("conform").setup({
     html = { "prettier" },
     css = { "prettier" },
     shell = { "shfmt" },
-    ts = { "ts-standard" }
+    ts = { "ts-standard" },
+    lua = { "stylua" }
   },
   -- If this is set, Conform will run the formatter on save.
   -- It will pass the table to conform.format().
   -- This can also be a function that returns the table.
-  -- format_on_save = {
-  -- I recommend these options. See :help conform.format for details.
-  -- lsp_fallback = true,
-  -- timeout_ms = 100,
-  -- },
+  format_on_save = {
+    -- I recommend these options. See :help conform.format for details.
+    lsp_fallback = true,
+    timeout_ms = 100,
+  },
   -- If this is set, Conform will run the formatter asynchronously after save.
   -- It will pass the table to conform.format().
   -- This can also be a function that returns the table.
-  format_after_save = {
-    lsp_fallback = true,
-  },
+  -- format_after_save = {
+  --   lsp_fallback = true,
+  -- },
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
