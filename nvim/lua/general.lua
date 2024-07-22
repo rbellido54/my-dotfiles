61f79f4583d0a08df47e3f73240441a44652856a
Ron B <35236981+ronb54@users.noreply.github.com>
2024-07-22 11:22:14 -0700

    Show tabs and spaces visually
    Signed-off-by: Ron B <35236981+ronb54@users.noreply.github.com>

--- nvim/lua/general.lua
+++ nvim/lua/general.lua
@@ -11,8 +11,13 @@ local disabled_built_ins = {
 	"man",
 	"tarPlugin",
 	"zipPlugin",
-	"gzip"
+	"gzip",
 }
+
+-- Show tabs and trailing spaces
+vim.opt.list = true
+vim.opt.listchars:append("tab:▸ ")
+vim.opt.listchars:append("trail:·")
 
 for i = 1, table.maxn(disabled_built_ins) do
 	vim.g["loaded_" .. disabled_built_ins[i]] = 1
