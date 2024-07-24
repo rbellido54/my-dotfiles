83ed3419f8aa662880ab326114b90e014d68642a
Ron B <35236981+ronb54@users.noreply.github.com>
2024-07-24 16:42:26 -0700

    Create wezterm config
    Signed-off-by: Ron B <35236981+ronb54@users.noreply.github.com>

--- .path-exports.zsh
+++ .path-exports.zsh
@@ -6,3 +6,7 @@ export NVM_DIR="$HOME/.nvm"
   [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 export PATH="/opt/cloud66/bin:${PATH}"
+
+# Wezterm cli
+# (MacOS)
+export PATH="$PATH:/Applications/WezTerm.app/Contents/MacOS"
