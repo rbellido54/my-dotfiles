955999ed6890c7ab6712109b89a95e8ab6a97bc6
Ron B <35236981+ronb54@users.noreply.github.com>
2024-07-24 19:40:25 -0700

    Add themes I like
    Signed-off-by: Ron B <35236981+ronb54@users.noreply.github.com>

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,12 +1,27 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+-- Dark themes
+-- config.color_scheme = "Everforest Dark (Gogh)"
+-- config.color_scheme = "Nova (base16)"
+-- config.color_scheme = "Obsidian (Gogh)"
+-- config.color_scheme = "PaleNightHC"
+-- config.color_scheme = "Palenight (Gogh)"
+-- config.color_scheme = "Pali (Gogh)"
+-- config.color_scheme = "Poimandres Storm"
+-- config.color_scheme = "Poimandres"
+config.color_scheme = "Ryuuko"
+-- config.color_scheme = "Seafoam Pastel"
+-- config.color_scheme = "Slate (Gogh)"
+-- config.color_scheme = "Spacegray Eighties (Gogh)"
 
-config.color_scheme = "Everforest Dark (Gogh)"
+-- Light themes for quick switching
+-- config.color_scheme = "One Light (Gogh)"
+-- config.color_scheme = "OneHalfLight"
+-- config.color_scheme = "Papercolor Light (Gogh)"
+-- config.color_scheme = "rose-pine-dawn"
+-- config.color_scheme = "Tomorrow"
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 13.0
@@ -14,8 +29,7 @@ config.font_size = 13.0
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.97
 config.macos_window_background_blur = 10
 
--- and finally, return the configuration to wezterm
 return config
