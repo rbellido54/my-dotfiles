24484230a6376a1da0eb30a2a6b4226205665e1d
Ron B <35236981+ronb54@users.noreply.github.com>
2024-09-13 19:03:07 -0700

    Change font + wezterm theme
    Signed-off-by: Ron B <35236981+ronb54@users.noreply.github.com>

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,13 +3,18 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Dark themes
--- config.color_scheme = "Everforest Dark (Gogh)"
+-- config.color_scheme = "Catppuccin Frappe"
+-- config.color_scheme = "Catppuccin Macchiato"
+-- config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Everforest Dark (Gogh)"
+-- config.color_scheme = "Everforest Dark Medium (Gogh)" -- nightly
+-- config.color_scheme = "Everforest Dark Soft (Gogh)" -- nightly
 -- config.color_scheme = "Nova (base16)"
 -- config.color_scheme = "Obsidian (Gogh)"
 -- config.color_scheme = "PaleNightHC"
 -- config.color_scheme = "Palenight (Gogh)"
 -- config.color_scheme = "Pali (Gogh)"
-config.color_scheme = "Poimandres Storm"
+-- config.color_scheme = "Poimandres Storm"
 -- config.color_scheme = "Poimandres"
 -- config.color_scheme = "Ryuuko"
 -- config.color_scheme = "Seafoam Pastel"
@@ -17,14 +22,16 @@ config.color_scheme = "Poimandres Storm"
 -- config.color_scheme = "Spacegray Eighties (Gogh)"
 
 -- Light themes for quick switching
+-- config.color_scheme = "Catppuccin Latte"
 -- config.color_scheme = "One Light (Gogh)"
 -- config.color_scheme = "OneHalfLight"
 -- config.color_scheme = "Papercolor Light (Gogh)"
 -- config.color_scheme = "rose-pine-dawn"
 -- config.color_scheme = "Tomorrow"
 
-config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
-config.font_size = 13.5
+config.font = wezterm.font("FiraMono Nerd Font Mono", { weight = "Medium" })
+config.font_size = 13.0
+config.line_height = 1.4
 
 config.enable_tab_bar = false
 
