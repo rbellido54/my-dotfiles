b854522a8ba9fbc5004ba1c129459fc4a2d67cc0
Ron B <35236981+ronb54@users.noreply.github.com>
2024-12-02 15:17:26 -0800

    copilot settings update
    Signed-off-by: Ron B <35236981+ronb54@users.noreply.github.com>

--- nvim/lua/plug-config/copilot.lua
+++ nvim/lua/plug-config/copilot.lua
@@ -176,66 +176,65 @@
 --   },
 -- })
 
-
 -- Copilot autosuggestions
 vim.g.copilot_hide_during_completion = 0
 vim.g.copilot_proxy_strict_ssl = 0
 
-vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
-  expr = true,
-  replace_keycodes = false
+vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+	expr = true,
+	replace_keycodes = false,
 })
 vim.g.copilot_no_tab_map = true
 
-vim.keymap.set('i', '<M-c>', '<Plug>(copilot-accept-word)')
-vim.keymap.set('i', '<M-C-c>', '<Plug>(copilot-accept-line)')
+vim.keymap.set("i", "<M-c>", "<Plug>(copilot-accept-word)")
+vim.keymap.set("i", "<M-C-c>", "<Plug>(copilot-accept-line)")
 
-
-local chat = require('CopilotChat')
+local chat = require("CopilotChat")
 
 chat.setup({
-  debug = true,
-  question_header = '',
-  answer_header = '',
-  error_header = '',
-  allow_insecure = true,
-  mappings = {
-    submit_prompt = {
-      insert = '',
-    },
-    reset = {
-      normal = '',
-      insert = '',
-    },
-  },
-  prompts = {
-    Explain = {
-      mapping = '<leader>ae',
-      description = 'AI Explain',
-    },
-    Review = {
-      mapping = '<leader>ar',
-      description = 'AI Review',
-    },
-    Tests = {
-      mapping = '<leader>at',
-      description = 'AI Tests',
-    },
-    Fix = {
-      mapping = '<leader>af',
-      description = 'AI Fix',
-    },
-    Optimize = {
-      mapping = '<leader>ao',
-      description = 'AI Optimize',
-    },
-    Docs = {
-      mapping = '<leader>ad',
-      description = 'AI Documentation',
-    },
-    CommitStaged = {
-      mapping = '<leader>ac',
-      description = 'AI Generate Commit',
-    },
-  },
+	model = "gpt-4o-2024-08-06",
+	debug = true,
+	question_header = "",
+	answer_header = "",
+	error_header = "",
+	allow_insecure = true,
+	mappings = {
+		submit_prompt = {
+			insert = "",
+		},
+		reset = {
+			normal = "",
+			insert = "",
+		},
+	},
+	prompts = {
+		Explain = {
+			mapping = "<leader>ae",
+			description = "AI Explain",
+		},
+		Review = {
+			mapping = "<leader>ar",
+			description = "AI Review",
+		},
+		Tests = {
+			mapping = "<leader>at",
+			description = "AI Tests",
+		},
+		Fix = {
+			mapping = "<leader>af",
+			description = "AI Fix",
+		},
+		Optimize = {
+			mapping = "<leader>ao",
+			description = "AI Optimize",
+		},
+		Docs = {
+			mapping = "<leader>ad",
+			description = "AI Documentation",
+		},
+		CommitStaged = {
+			mapping = "<leader>ac",
+			description = "AI Generate Commit",
+		},
+	},
 })
