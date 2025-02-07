6d7aff1bfb0fed786e00f1efebf8614ec46617a5
Ron B <35236981+ronb54@users.noreply.github.com>
2025-02-06 18:53:38 -0800

    Create post and pre install scripts
    Signed-off-by: Ron B <35236981+ronb54@users.noreply.github.com>

--- post-install.sh
+++ post-install.sh
@@ -1,14 +1,8 @@
 #!bin/bash
 
-python3 -m pip install --user --upgrade pynvim
 
-# rbenv
-echo 'eval "$(~/.rbenv/bin/rbenv init - zsh)"' >> ~/.zshrc
-
-# nodeenv
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-
-# oh-my-zsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+# Install languages
+mise use --global node@23
+mise use --global python@3
+mise use --global ruby@3
+mise use --global go@1
