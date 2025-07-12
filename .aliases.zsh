33f614e44da199c1bab4c2185ed5fd68be055b68
Ron B <35236981+ronb54@users.noreply.github.com>
2025-07-12 11:08:44 -0700

    Alias tailscale
    Signed-off-by: Ron B <35236981+ronb54@users.noreply.github.com>

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,7 +1,12 @@
 alias vim="nvim"
 alias n="nvim"
 alias reload_zshrc="exec zsh"
-alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 
 alias -g grep="egrep"
 alias -g cat="bat"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    command -v /Applications/Tailscale.app/Contents/MacOS/Tailscale >/dev/null 2>&1 && alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    command -v tailscale >/dev/null 2>&1 && alias tailscale="tailscale"
+fi
