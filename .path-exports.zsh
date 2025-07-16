671c968623406bf92cec26e7348f5f288e370741
Ron Bellido <35236981+ronb54@users.noreply.github.com>
2025-07-15 23:04:47 -0700

    Use ssh in gpg and make gpg manage the ssh keys

--- .path-exports.zsh
+++ .path-exports.zsh
@@ -1,4 +1,4 @@
 export PKG_CONFIG_PATH="/opt/homebrew/opt/libpq/lib/pkgconfig"
 export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
 export PATH="$HOME/go/bin:$PATH"
-export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+#export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
