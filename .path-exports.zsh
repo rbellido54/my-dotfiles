7450d890730dbf94afaa896fb7055d2a195d6249
Ron Bellido <35236981+ronb54@users.noreply.github.com>
2025-08-04 11:03:04 -0700

    path-exports + tmux.conf for linux

--- .path-exports.zsh
+++ .path-exports.zsh
@@ -1,4 +1,14 @@
-export PKG_CONFIG_PATH="/opt/homebrew/opt/libpq/lib/pkgconfig"
-export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
-export PATH="$HOME/go/bin:$PATH"
-#export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+# Set complete path
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  export PKG_CONFIG_PATH="/opt/homebrew/opt/libpq/lib/pkgconfig"
+  export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
+  export PATH="$HOME/go/bin:$PATH"
+else
+  export PATH="./bin:$HOME/.local/bin:$HOME/.local/share/omarchy/bin:$PATH"
+fi
+set +h
+
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+
+# Omarchy path
+export OMARCHY_PATH="/home/$USER/.local/share/omarchy"
